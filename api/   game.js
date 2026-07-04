dd500ac281746f4f497297aeb316e05c41887961
gs42dmb92g-commits <gs42dmb92g@privaterelay.appleid.com>
2026-07-04 19:49:12 +0300

    Update    game.js

--- api/   game.js
+++ api/   game.js
@@ -1,15 +1,3 @@
-module.exports = async (req, res) => {
-  const { placeId } = req.query;
-
-  try {
-    const r = await fetch(
-      `https://games.roblox.com/v1/games?universeIds=${placeId}`
-    );
-
-    const text = await r.text();
-
-    return res.status(200).send(text);
-  } catch (e) {
-    return res.status(500).send(String(e));
-  }
+module.exports = (req, res) => {
+  res.status(200).json({ ok: true });
 };
