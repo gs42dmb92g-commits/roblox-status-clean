29b6f14b54636aa6ecfac1ccf93c01c4a7f44e42
gs42dmb92g-commits <gs42dmb92g@privaterelay.appleid.com>
2026-07-04 19:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { placeId } = req.query;
 
-  if (!placeId) return res.json({ name: "missing", players: 0 });
-
   try {
-    const r = await fetch(
-      `https://games.roblox.com/v1/games?universeIds=${placeId}`
+    // 1. universe al
+    const u = await fetch(
+      `https://apis.roblox.com/universes/v1/places/${placeId}/universe`
     );
 
-    const d = await r.json();
-    const g = d?.data?.[0];
+    const ujson = await u.json();
+    const universeId = ujson.universeId;
 
-    return res.json({
-      name: g?.name || "unknown",
-      players: g?.playing || 0
+    // 2. game çek
+    const g = await fetch(
+      `https://games.roblox.com/v1/games?universeIds=${universeId}`
+    );
+
+    const data = await g.json();
+    const game = data?.data?.[0];
+
+    res.json({
+      name: game?.name || "unknown",
+      players: game?.playing || 0
     });
 
   } catch (e) {
-    return res.json({ name: "error", players: 0 });
+    res.json({
+      name: "error",
+      players: 0
+    });
   }
-}
+};
